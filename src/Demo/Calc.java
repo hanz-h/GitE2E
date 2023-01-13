da83fd8148a5c3db35289734fb95e4207c4a6295
Hemmy <hemmy@topgrep.com>
2023-01-13 08:29:18 +0530

    First Commit for Git E2E demo

---
+++ src/Demo/Calc.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a demo for Git E2E");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitE2E {
+}
